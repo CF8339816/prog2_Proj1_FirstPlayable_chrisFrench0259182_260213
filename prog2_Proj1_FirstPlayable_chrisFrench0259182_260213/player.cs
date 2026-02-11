49f875671751bdb12146b060752d8809c573ef71
CF8339816 <chris_french_3dga@hotmail.com>
2026-02-11 10:30:26 -0400

    more code stuff

--- prog2_Proj1_FirstPlayable_chrisFrench0259182_260213/player.cs
+++ prog2_Proj1_FirstPlayable_chrisFrench0259182_260213/player.cs
@@ -9,14 +9,50 @@ namespace prog2_Proj1_FirstPlayable_chrisFrench0259182_260213
 {
     public class Player : Character
     {
+        static int p1_x_input;
+        static int p1_y_input;
+        static int p1_x_pos;
+        static int p1_y_pos;
+        static (int, int) p1_min_max_x = (1, 55);
+        static (int, int) p1_min_max_y = (1, 29);
+        static int p1_Old_X;
+        static int p1_Old_Y;
         public void plSpawnLoc()
         { 
            base._x = 3;
           base._y = 3;
+           base._symbol = "@";
         }
-        _symbol= 
+    
 
+static void ProcessInput()
+        {
+            p1_x_input = 0;
+            p1_y_input = 0;
 
+            ConsoleKey input = Console.ReadKey(true).Key;
+
+            if (input == ConsoleKey.A) p1_x_input = -1;
+            if (input == ConsoleKey.D) p1_x_input = 1;
+            if (input == ConsoleKey.W) p1_y_input = -1;
+            if (input == ConsoleKey.S) p1_y_input = 1;
+
+            
+            int nextX = p1_x_pos + p1_x_input;
+            int nextY = p1_y_pos + p1_y_input;
+
+          if (_canMoveTo= true)
+            {
+                p1_Old_X = p1_x_pos;
+                p1_Old_Y = p1_y_pos;
+            }
+            else
+            {
+                p1_x_input = 0;
+                p1_y_input = 0;
+            }
+
+        }
 
     }
 }
